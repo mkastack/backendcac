e65af1b0f5f929e596be6b50633121bd6d612b8c
CACI Admin <admin@cacbubiashie.org>
2026-04-15 22:39:34 +0000

    Add contact number for Women Ministry

---
+++ supabase/migrations/20260415000004_update_women_ministry_contact.sql
@@ -0,0 +1,3 @@
+-- Update women ministry contact information
+UPDATE ministries SET contact = '+233595232835' WHERE name ILIKE '%women%';
+UPDATE ministries SET contact = '+233595232835' WHERE name ILIKE '%female%';
